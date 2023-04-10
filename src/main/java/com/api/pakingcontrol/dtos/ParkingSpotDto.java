d1e0b7daee97a1158a7c96bf35497d7d6ba581bd
Italo Oliveira <italooliveira-xd@hotmail.com>
2023-04-09 23:23:09 -0300

    Adicionando annotation para validaçãon o dto

--- src/main/java/com/api/pakingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/pakingcontrol/dtos/ParkingSpotDto.java
@@ -1,6 +1,25 @@
 package com.api.pakingcontrol.dtos;
 
-public record ParkingSpotDto(String parkingSpotNumber, String licensePlateCar, String brandCar, String modelCar,
-		String colorCar, String responsibleName, String apartment, String block) {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public record ParkingSpotDto(
+		@NotBlank
+		String parkingSpotNumber,
+		@NotBlank
+		@Size(max = 7)
+		String licensePlateCar,
+		@NotBlank
+		String brandCar,
+		@NotBlank
+		String modelCar,
+		@NotBlank
+		String colorCar,
+		@NotBlank
+		String responsibleName,
+		@NotBlank
+		String apartment,
+		@NotBlank
+		String block) {
 
 }
